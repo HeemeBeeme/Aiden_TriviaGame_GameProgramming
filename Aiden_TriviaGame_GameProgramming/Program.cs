ae7eda53d7a8530fb389e4e7021db7d3723ad66c
Heeme Beeme <heemebeeme@gmail.com>
2025-11-25 15:08:07 -0400

    Initial commit

---
+++ Aiden_TriviaGame_GameProgramming/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiden_TriviaGame_GameProgramming
+{
+    internal class Program
+    {
+        static string PlayerInput;
+        static string PlayerName;
+        static int PlayerScore = 0;
+
+        static int QuestionNum;
+
+        static string[] questionsArray = new string[] {"Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10" };
+
+        static string[,] answersArray = new string[,] {
+                                                        { "Q1A1", "Q1A2", "Q1A3", "Q1A4" },
+                                                        { "Q2A1", "Q2A2", "Q2A3", "Q2A4" },
+                                                        { "Q3A1", "Q3A2", "Q3A3", "Q3A4" },
+                                                        { "Q4A1", "Q4A2", "Q4A3", "Q4A4" },
+                                                        { "Q1A1", "Q1A2", "Q1A3", "Q1A4" },
+                                                        { "Q1A1", "Q1A2", "Q1A3", "Q1A4" },
+                                                        { "Q1A1", "Q1A2", "Q1A3", "Q1A4" },
+                                                        { "Q1A1", "Q1A2", "Q1A3", "Q1A4" },
+                                                        { "Q1A1", "Q1A2", "Q1A3", "Q1A4" },
+                                                        { "Q1A1", "Q1A2", "Q1A3", "Q1A4" } };
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
