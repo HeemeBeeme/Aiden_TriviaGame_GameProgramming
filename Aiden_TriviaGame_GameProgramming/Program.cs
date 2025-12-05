b88cd969f9048af07fe48b7c4567b92e0c3c7095
Heeme Beeme <heemebeeme@gmail.com>
2025-12-05 13:32:24 -0400

    Color, easter egg, Q5-10

--- Aiden_TriviaGame_GameProgramming/Program.cs
+++ Aiden_TriviaGame_GameProgramming/Program.cs
@@ -15,19 +15,28 @@ namespace Aiden_TriviaGame_GameProgramming
         static string PlayerScoreStatus;
         static int PlayerScore = 0;
 
-        static string[] questionsArray = new string[] { "What Is A String?", "How Do You Write \"Hello World\" In The Console?", "What Is A Comment?", "What Is Void Used For In A Method?", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10" };
+        static string[] questionsArray = new string[] { "What Is A String?",
+                                                        "How Do You Write \"Hello World\" In The C# Console?",
+                                                        "What Is A Comment?",
+                                                        "What Is Void Used For In A C# Method?",
+                                                        "What Is C#?",
+                                                        "What Is A Method?",
+                                                        "What Is An Int?",
+                                                        "What Is A Float?",
+                                                        "How Do You Write An Int Tuple In C#?",
+                                                        "What Is String.Length?" };
 
         static string[,] answersArray = new string[,] {
                                                         { "A Sequence Of Characters", "A Single Character", "A Number", "That Stuff On Guitars" },
                                                         { "System.out.println(\"Hello World\");", "printf(\"Hello World\");", "Console.WriteLine(\"Hello World\")", "print(\"Hello World\")" },
                                                         { "A Value", "A Line(s) Within Code That Is Ignored", "A List Of Numbers", "A Method" },
                                                         { "The Return Type", "A Variable Type", "Another Term For Null", "The Blank Space Between Lines Of Code" },
-                                                        { "Q5A1", "Q5A2", "Q5A3", "Q5A4" },
-                                                        { "Q6A1", "Q6A2", "Q6A3", "Q6A4" },
-                                                        { "Q7A1", "Q7A2", "Q7A3", "Q7A4" },
-                                                        { "Q8A1", "Q8A2", "Q8A3", "Q8A4" },
-                                                        { "Q9A1", "Q9A2", "Q9A3", "Q9A4" },
-                                                        { "Q10A1", "Q10A2", "Q10A3", "Q10A4" } };
+                                                        { "A Computer Company", "A Coding Application", "A Variable Type", "A Coding Language" },
+                                                        { "A String Used For Storing Numbers", "Another Term For Class", "A Code Block That Contains A Series Of Statements", "A Way Of Doing Things" },
+                                                        { "A Variable Type Used To Store Whole Numbers", "A Variable Type Used To Store Decimal Numbers", "A List Of Numbers", "A Class" },
+                                                        { "A Type Of String", "A Variable Type Used To Store Characters", "A Method To Skip Lines Of Code", "A Variable Type Used To Store Decimal Numbers" },
+                                                        { "Tuple(int)(int)", "(int, int)", "int, int", "Tuple.((int)(int))" },
+                                                        { "A List Of Strings", "The Amount Of Characters In A String", "The Amount Of Words In A String", "An Amount Of Numbers Within A String" } };
 
         static string[] correctAnswersArray = new string[] { "1", "3", "2", "1", "4", "3", "1", "4", "2", "2" };
 
@@ -50,6 +59,23 @@ namespace Aiden_TriviaGame_GameProgramming
             {
                 Environment.Exit(0);
             }
+            else if(PlayerInput == "MAYBE")
+            {
+                Console.CursorVisible = false;
+                Console.Clear();
+                Console.WriteLine("Get Out");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("Get Out.");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("Get Out..");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("Get Out...");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
             else
             {
                 Console.Clear();
@@ -61,6 +87,8 @@ namespace Aiden_TriviaGame_GameProgramming
 
         static void DisplayScore()
         {
+            ConsoleColor(System.ConsoleColor.Gray);
+
             if (PlayerScore == 10)
             {
                 PlayerScoreStatus = "You Must Be a Genious!";
@@ -86,7 +114,13 @@ namespace Aiden_TriviaGame_GameProgramming
                 PlayerScoreStatus = "Oh Uh... Better Luck Next Time?";
             }
 
-            Console.WriteLine($"{PlayerName}! Your Score Was: {PlayerScore}/{questionsArray.Length}!\n");
+            ConsoleColor(System.ConsoleColor.Green);
+            Console.Write($"{PlayerName}! ");
+            ConsoleColor(System.ConsoleColor.Gray);
+            Console.Write($"Your Score Was: ");
+            ConsoleColor(System.ConsoleColor.Yellow);
+            Console.Write($"{PlayerScore}/{questionsArray.Length}!\n\n");
+            ConsoleColor(System.ConsoleColor.Gray);
             Console.WriteLine(PlayerScoreStatus);
             Console.WriteLine("\nPress Any Key...");
             Console.CursorVisible = false;
@@ -95,9 +129,19 @@ namespace Aiden_TriviaGame_GameProgramming
             PlayAgain();
         }
 
+        static void ConsoleColor(ConsoleColor consoleColor)
+        {
+            Console.ForegroundColor = consoleColor;
+        }
+
         static void HUD(int QuestionNum)
         {
-            Console.WriteLine($"{PlayerName}: {PlayerScore}, Question: {QuestionNum + 1}\n");
+            ConsoleColor(System.ConsoleColor.Green);
+            Console.WriteLine($"{PlayerName}");
+            ConsoleColor(System.ConsoleColor.Yellow);
+            Console.Write($"Score: {PlayerScore}");
+            ConsoleColor(System.ConsoleColor.Gray);
+            Console.Write($", Question: {QuestionNum + 1}/{questionsArray.Length}\n\n");
         }
 
         static void Questions()
@@ -106,15 +150,19 @@ namespace Aiden_TriviaGame_GameProgramming
             {
                 HUD(i);
 
+                ConsoleColor(System.ConsoleColor.Gray);
                 Console.WriteLine(questionsArray[i]);
 
+                ConsoleColor(System.ConsoleColor.Cyan);
                 Console.WriteLine($"\n1:{answersArray[i, 0]}");
                 Console.WriteLine($"2:{answersArray[i, 1]}");
                 Console.WriteLine($"3:{answersArray[i, 2]}");
                 Console.WriteLine($"4:{answersArray[i, 3]}");
 
+                ConsoleColor(System.ConsoleColor.Gray);
                 Console.WriteLine("\nPlease Enter Your Answer: ");
-                Console.SetCursorPosition(26, 9);
+                Console.SetCursorPosition(26, 10);
+                ConsoleColor(System.ConsoleColor.Cyan);
                 PlayerInput = Console.ReadLine().ToUpper();
 
                 if(PlayerInput.Length > 1 || !int.TryParse(PlayerInput, out int j) || int.Parse(PlayerInput) > 4 || int.Parse(PlayerInput) < 1)
